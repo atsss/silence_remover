0fd8fbc3beaf52948f249352f144145925277207
atsss <atsushi0623itoh@gmail.com>
2024-02-26 10:31:19 +0000

    Create player prototype

--- app/index.js
+++ app/index.js
@@ -1,9 +1,26 @@
-fetch('./audio_metadata.json')
-    .then((response) => response.json())
-    .then((json) => {
-      const $container = $('.js-audio-list')
-      for(const [index, data] of json.entries()) {
-        const child = $(`<div><div>${data["start_sec"]} - ${data["end_sec"]}</div><audio controls><source src="./assets/active-${index}.wav"></audio></div>`);
-        $container.append(child);
-      }
+(async function () {
+  const response = await fetch('./audio_metadata.json')
+  const metadata = await response.json()
+
+  const video = document.getElementById('js-video');
+
+  function playAudioAfterVideo(index) {
+    video.pause()
+    const audio = document.createElement('audio');
+    audio.src = `./assets/active-${index}.wav`;
+    audio.addEventListener('ended', function() {
+        video.play();
     });
+    audio.play();
+  }
+
+  video.addEventListener('timeupdate', function(event) {
+    const currentTime = event.target.currentTime;
+    for (const [index, data] of metadata.entries()) {
+      if (Math.abs(Math.floor(currentTime*10)/10 - data['start_sec']) < 0.1) {
+        console.info(index, data);
+        playAudioAfterVideo(index)
+      };
+    };
+  });
+}());
